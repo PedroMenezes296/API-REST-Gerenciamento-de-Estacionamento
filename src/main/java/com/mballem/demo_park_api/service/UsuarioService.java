27b6f482630805138dd6994fe1cbc8d1e6b43cb9
PedroMenezes296 <barananomenezespedro@gmail.com>
2026-04-29 14:24:42 -0300

    feat(usuario): implementa criação, busca por ID e atualização de senha

--- src/main/java/com/mballem/demo_park_api/service/UsuarioService.java
+++ src/main/java/com/mballem/demo_park_api/service/UsuarioService.java
@@ -1,8 +1,10 @@
 package com.mballem.demo_park_api.service;
 
+import com.mballem.demo_park_api.entity.Usuario;
 import com.mballem.demo_park_api.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @RequiredArgsConstructor
@@ -15,4 +17,23 @@ public class UsuarioService {
     private final UsuarioRepository usuarioRepository;
 
 
+    @Transactional
+    public Usuario salvar(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+    @Transactional(readOnly = true) // indicado pro spring que o metodo é exclusivo para consulta
+    public Usuario buscarPorId(Long id) {
+        return usuarioRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("Usuário não encontrado.")
+        );
+    }
+
+    @Transactional
+    public Usuario editarSenha(Long id, String password) {
+        Usuario user = buscarPorId(id);
+        user.setPassword(password);
+        return user;
+        //não precisa de um usuarioRepository pois o hibernate ja entende que é um dado persistente
+    }
 }
--- src/main/java/com/mballem/demo_park_api/web/controller/UsuarioController.java
+++ src/main/java/com/mballem/demo_park_api/web/controller/UsuarioController.java
@@ -1,9 +1,11 @@
 package com.mballem.demo_park_api.web.controller;
 
+import com.mballem.demo_park_api.entity.Usuario;
 import com.mballem.demo_park_api.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -12,4 +14,28 @@ public class UsuarioController {
 
     private final UsuarioService usuarioService;
 
+    // encapsular o objeto ususario e dps vai ser tranformado em json e enviado para o cliente,
+    // criar um json com os campos do objeto usuario
+    @PostMapping
+    public ResponseEntity<Usuario> create(@RequestBody Usuario usuario){
+        Usuario user = usuarioService.salvar(usuario);
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+        //codigo 201
+    }
+
+    @GetMapping("/{id}") //estamos enviando um valor que seria o id que queremos
+    public ResponseEntity<Usuario> getById(@PathVariable Long id){
+        // @PathVariable vai pegar o json e passar para long
+        Usuario user = usuarioService.buscarPorId(id);
+        return ResponseEntity.ok().body(user);
+        //codigo 201
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Usuario> updatePassword(@PathVariable Long id, @RequestBody Usuario usuario){
+        // @PathVariable vai pegar o json e passar para long
+        Usuario user = usuarioService.editarSenha(id, usuario.getPassword());
+        return ResponseEntity.ok().body(user);
+        //codigo 201
+    }
 }
