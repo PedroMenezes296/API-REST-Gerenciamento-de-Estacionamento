2c6a67e9163ee9f673995581206e7e59f72b213d
PedroMenezes296 <barananomenezespedro@gmail.com>
2026-05-08 09:09:18 -0300

    feat(UsuarioCreateDto): implementado validação para senha (6 char) e email formato definido

--- src/main/java/com/mballem/demo_park_api/web/controller/UsuarioController.java
+++ src/main/java/com/mballem/demo_park_api/web/controller/UsuarioController.java
@@ -6,6 +6,7 @@ import com.mballem.demo_park_api.web.dto.UsuarioCreateDto;
 import com.mballem.demo_park_api.web.dto.UsuarioResponseDto;
 import com.mballem.demo_park_api.web.dto.UsuarioSenhaDto;
 import com.mballem.demo_park_api.web.dto.mapper.UsuarioMapper;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,7 +24,7 @@ public class UsuarioController {
     // encapsular o objeto ususario e dps vai ser tranformado em json e enviado para o cliente,
     // criar um json com os campos do objeto usuario
     @PostMapping
-    public ResponseEntity<UsuarioResponseDto> create(@RequestBody UsuarioCreateDto CreateDto){
+    public ResponseEntity<UsuarioResponseDto> create(@Valid @RequestBody UsuarioCreateDto CreateDto){
         Usuario user = usuarioService.salvar(UsuarioMapper.toUsuario(CreateDto));
         return ResponseEntity.status(HttpStatus.CREATED).body(UsuarioMapper.toDto(user));
         //codigo 201
